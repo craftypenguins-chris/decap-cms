5ba154175a4e3bbda98baa94ec5e1750351ae31a
Chris Pickett <chris@craftypenguins.net>
2025-09-20 22:33:53 +0000

    Media Folder via Local Preview Server

--- packages/decap-cms-core/src/components/Editor/Editor.js
+++ packages/decap-cms-core/src/components/Editor/Editor.js
@@ -234,7 +234,12 @@ export class Editor extends React.Component {
       entryDraft,
     } = this.props;
 
-    await persistEntry(collection);
+    try {
+      await persistEntry(collection);
+    } catch (e) {
+      try { console.error('[editor] persist error', e); } catch (_) {}
+      return; // stop on error to avoid unhandled rejections and follow-up actions
+    }
 
     this.deleteBackup();
 
